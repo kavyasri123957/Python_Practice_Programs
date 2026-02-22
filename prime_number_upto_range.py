6bac348568db98e58450209b4b2de4ac34fc341c
kavyasri123957 <srinissanki@gmail.com>
2026-02-22 10:53:40 +0530

    Add files via upload

---
+++ prime_number_upto_range.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number "))
+for i in range(2,num+1):
+    is_prime=True
+    for j in range(2,i):
+        if i%j==0:
+             is_prime=False
+             break
+    if (is_prime):
+        print(i)
